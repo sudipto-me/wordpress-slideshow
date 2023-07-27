7ebb693eb34facd8d742f281a184aadbffd0e1f3
Sudipto Shakhari <sudipto.shakhari@selise.ch>
2023-07-27 13:44:23 +0600

    initial commit

---
+++ wordpress-slideshow.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: WordPress-Slideshow Plugin
+ * Plugin URI:  https://www.pluginever.com/plugins/wocommerce-serial-numbers-pro/
+ * Description: This will show slideshows on the wordpress frontend.
+ * Version:     1.0.0
+ * Author:      Sudipto Shakhari
+ * Author URI:  https://shakahri.cc/
+ * License:     GPLv2+
+ * Text Domain: wordpress-slideshow
+ * Domain Path: /languages
+ * Tested up to: 6.2
+ *
+ * @package WordPressSlideShow
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
